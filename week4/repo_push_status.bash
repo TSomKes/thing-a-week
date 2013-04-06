1000f28d7f7c92daa2d0f9c5284c3908e004a21f
TSomKes <tsomkes@gmail.com>
2013-04-06 17:08:31 -0500

    Changed from direct echo's to status variable
    - Changed variable case to lower (sounds like built-in variables are all
      upper case)
    - Added comments

--- week4/repo_push_status.bash
+++ week4/repo_push_status.bash
@@ -1,15 +1,42 @@
 #!/usr/bin/env bash
 
-CODE_DIR=~/code/
+code_dir=~/code/
+
+git_no_changes_message="Everything up-to-date"
 
 # Find all project directories immediately under code folder
-for dir in `find $CODE_DIR -maxdepth 1 -mindepth 1 -type d`
+for dir in `find $code_dir -maxdepth 1 -mindepth 1 -type d`
 do
+	# Move into $dir, in case we find a repo, so that the git/hg commands work
+	# relative to the working directory
+	cd $dir
+
+	status=$dir
+
+	# .git directory found?  Git repo.
 	if [ -d $dir"/.git" ]; then
-		echo $dir "- Git"
+
+		status+="- Git - "
+		git_output=`git push --dry-run 2>&1`
+
+		if [[ "$git_output" == "$git_no_changes_message" ]]; then
+			status+="sync'd"
+		else 
+			status+="un-sync'd local changes found"
+		fi
+
+	# .hg directory found?  Hg repo.
 	elif [ -d $dir"/.hg" ]; then
-		echo $dir "- Hg"
+		status+="- Hg -"
+		hg_output=`hg outgoing`
+		echo $hg_output
+
 	else
 		echo $dir "- no repo found"
 	fi
+
+	echo $status
+
+	# Move back to the original directory, just 'cause.
+	cd $code_dir
 done
